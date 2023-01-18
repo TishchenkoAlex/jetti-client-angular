8a3c9a17070739524704aa4a5fbcca8d8142b8f4
gimpon <dbez@outlook.com>
2023-01-18 15:56:51 +0200

    del flex - deprecated

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,8 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   exports: [
-    FlexLayoutModule,
     A11yModule,
   ]
 })
